190085b1d73c8933c7bfcac92aada6f2dee1b1d4
NamanAgarwal08 <naman.agarwal_cs21@gla.ac.in>
2025-03-08 13:38:45 +0530

    Add. Slf4j annotation used for creating logs on terminal

--- src/main/java/com/example/EmployeePayroll/controller/EmployeeController.java
+++ src/main/java/com/example/EmployeePayroll/controller/EmployeeController.java
@@ -9,8 +9,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
+@Slf4j
 @RestController
-@RequestMapping("/employeepayrollservice")
+@RequestMapping("/employee")
 public class EmployeeController {
 
     @Autowired
@@ -18,34 +19,34 @@ public class EmployeeController {
 
     @GetMapping("/get/{id}")
     public EmployeeDTO get(@PathVariable Long id){
-
+        log.info("Fetching employee details corresponding to id : {}", id);
         return employeeService.get(id);
     }
 
     @PostMapping("/create")
     public EmployeeDTO create(@RequestBody EmployeeDTO newEmp){
-
+        log.info("Creating new employee", newEmp);
         return employeeService.create(newEmp);
     }
 
     @PatchMapping("/edit/{id}")
     public EmployeeDTO edit(@RequestBody EmployeeDTO emp, @PathVariable Long id){
-
+        log.info("Editing employee details corresponding to id: {}",id);
         return employeeService.edit(emp, id);
     }
 
     @DeleteMapping("/delete/{id}")
     public String delete(@PathVariable Long id){
-
+        log.warn("Deleting employee details corresponding to id: {}", id);
         return employeeService.delete(id);
     }
 
 
-    //for list
-    @GetMapping("/all")  // ✅ New API to Fetch All Employees
-    public List<EmployeeDTO> getAllEmployees() {
-
-        return employeeService.getAllEmployees();
-    }
+//    //for list
+//    @GetMapping("/all")  // ✅ New API to Fetch All Employees
+//    public List<EmployeeDTO> getAllEmployees() {
+//        log.info("Fetching all employee details from list.");
+//        return employeeService.getAllEmployees();
+//    }
 
 }
